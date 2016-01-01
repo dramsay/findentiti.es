c971ce388625a5a92a12b8a2b696a01b3599f603
Doug Ramsay <dougramsay@gmail.com>
2015-12-31 21:55:52 -0500

    Initial commit from Yeoman Angular generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name findentitiesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the findentitiesApp
+ */
+angular.module('findentitiesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name findentitiesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the findentitiesApp
+ */
+angular.module('findentitiesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
